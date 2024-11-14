e5d4ad7cfb44a3ea6d0027bf5d6f6e3009a704c6
AyushShivhare79 <ayushshivhare1008@gmail.com>
2024-11-14 18:43:46 +0530

    fix: social-link tab load issue

--- pages/manage-links/ManageLinks.tsx
+++ pages/manage-links/ManageLinks.tsx
@@ -5,8 +5,34 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import DashboardLayout from "@/layouts/DashboardLayout";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import SocialLinks from "@/components/SocialLinks";
 import { useToast } from "@/hooks/use-toast";
+
+const ManageLinks = () => {
+  return (
+    <>
+      <DashboardLayout>
+        <div className="w-full">
+          <Tabs defaultValue="links" className="px-10 py-10">
+            <TabsList>
+              <TabsTrigger value="links">Links</TabsTrigger>
+              <TabsTrigger value="social">Socials</TabsTrigger>
+            </TabsList>
+
+            <TabsContent value="links">
+              {/* First component */}
+              <CustomLinks />
+            </TabsContent>
+            <TabsContent value="social">
+              {/* Second component */}
+              <SocialLinks />
+            </TabsContent>
+          </Tabs>
+        </div>
+      </DashboardLayout>
+    </>
+  );
+};
+export default ManageLinks;
 
 export interface LinkStateType {
   id: number;
@@ -14,7 +40,7 @@ export interface LinkStateType {
   url: string;
 }
 
-const ManageLinks = () => {
+const CustomLinks = () => {
   const [links, setLinks] = useState<LinkStateType[]>([]);
 
   const { toast } = useToast();
@@ -75,70 +101,184 @@ const ManageLinks = () => {
 
   return (
     <>
-      <DashboardLayout>
-        <div className="w-full">
-          <Tabs defaultValue="links" className="px-10 py-10">
-            <TabsList>
-              <TabsTrigger value="links">Links</TabsTrigger>
-              <TabsTrigger value="social">Socials</TabsTrigger>
-            </TabsList>
+      <div className="w-[50%]">
+        {links.map((value, index) => (
+          <div key={index} className="grid grid-cols-2 gap-5 pb-5">
+            <div>
+              <Label htmlFor={`link-name-${index}`}>Link name</Label>
+              <Input
+                type="text"
+                id={`link-name-${index}`}
+                value={value.name}
+                onChange={(e) => handleChange(index, "name", e.target.value)}
+                autoComplete="off"
+                placeholder="Exclusive Content"
+              />
+            </div>
 
-            <TabsContent value="links">
-              <div className="w-[50%]">
-                {links.map((value, index) => (
-                  <div key={index} className="grid grid-cols-2 gap-5 pb-5">
-                    <div>
-                      <Label htmlFor={`link-name-${index}`}>Link name</Label>
-                      <Input
-                        type="text"
-                        id={`link-name-${index}`}
-                        value={value.name}
-                        onChange={(e) =>
-                          handleChange(index, "name", e.target.value)
-                        }
-                        autoComplete="off"
-                        placeholder="Exclusive Content"
-                      />
-                    </div>
-
-                    <div>
-                      <Label htmlFor={`link-url-${index}`}>Link URL</Label>
-                      <div className="flex items-center gap-5">
-                        <Input
-                          type="text"
-                          id={`link-url-${index}`}
-                          value={value.url}
-                          onChange={(e) =>
-                            handleChange(index, "url", e.target.value)
-                          }
-                          autoComplete="off"
-                          placeholder="https://www.instagram.com/"
-                        />
-                        <Button onClick={() => handleDelete(value.id)}>
-                          Delete
-                        </Button>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-
-                <div className="flex justify-between">
-                  <Button onClick={handleAdd}>Add link</Button>
-                  <div className="flex gap-2">
-                    <Button onClick={handleCancel}>Cancel</Button>
-                    <Button onClick={handleUpdate}>Update</Button>
-                  </div>
-                </div>
+            <div>
+              <Label htmlFor={`link-url-${index}`}>Link URL</Label>
+              <div className="flex items-center gap-5">
+                <Input
+                  type="text"
+                  id={`link-url-${index}`}
+                  value={value.url}
+                  onChange={(e) => handleChange(index, "url", e.target.value)}
+                  autoComplete="off"
+                  placeholder="https://www.instagram.com/"
+                />
+                <Button onClick={() => handleDelete(value.id)}>Delete</Button>
               </div>
-            </TabsContent>
-            <TabsContent value="social">
-              <SocialLinks />
-            </TabsContent>
-          </Tabs>
+            </div>
+          </div>
+        ))}
+
+        <div className="flex justify-between">
+          <Button onClick={handleAdd}>Add link</Button>
+          <div className="flex gap-2">
+            <Button onClick={handleCancel}>Cancel</Button>
+            <Button onClick={handleUpdate}>Update</Button>
+          </div>
         </div>
-      </DashboardLayout>
+      </div>
     </>
   );
 };
 
-export default ManageLinks;
+export interface SocialStateType {
+  instagramUrl: string;
+  twitterUrl: string;
+  tiktokUrl: string;
+  telegramUrl: string;
+  snapchatUrl: string;
+  youtubeUrl: string;
+}
+const SocialLinks = () => {
+  const [social, setSocial] = useState<SocialStateType[]>([
+    {
+      instagramUrl: "",
+      twitterUrl: "",
+      tiktokUrl: "",
+      telegramUrl: "",
+      snapchatUrl: "",
+      youtubeUrl: "",
+    },
+  ]);
+
+  const { toast } = useToast();
+
+  const fetchData = async () => {
+    const response = await axios.get("/api/social");
+    setSocial(response.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleUpdate = async () => {
+    const response = await axios.post("/api/social", { social });
+    if (response.data.msg === "Update successful!") {
+      toast({
+        title: "Update successful!",
+      });
+    }
+    new Promise(() => {
+      setTimeout(() => {
+        fetchData();
+      }, 1000);
+    });
+  };
+
+  const handleCancel = () => {
+    fetchData();
+  };
+
+  const handleChange = (field: keyof SocialStateType, value: string) => {
+    if (social.length > 0) {
+      const updatedLinks = [...social];
+      updatedLinks[0] = { ...updatedLinks[0], [field]: value };
+      setSocial(updatedLinks);
+    }
+  };
+
+  return (
+    <>
+      <div className=" w-2/4 ">
+        {social.length > 0 && (
+          <div className="grid grid-cols-2 gap-5  pb-5">
+            <div>
+              <Label htmlFor="instagram">Instagram</Label>
+              <Input
+                id="instagram"
+                placeholder="https://www.instagram.com/"
+                autoComplete="off"
+                value={social[0].instagramUrl}
+                onChange={(e) => handleChange("instagramUrl", e.target.value)}
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="tiktok">Tiktok</Label>
+              <Input
+                id="tiktok"
+                placeholder="https://www.tiktok.com/"
+                autoComplete="off"
+                value={social[0].tiktokUrl}
+                onChange={(e) => handleChange("tiktokUrl", e.target.value)}
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="twitter">Twitter</Label>
+              <Input
+                id="twitter"
+                placeholder="https://www.twitter.com/"
+                autoComplete="off"
+                value={social[0].twitterUrl}
+                onChange={(e) => handleChange("twitterUrl", e.target.value)}
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="snapchat">Snapchat</Label>
+              <Input
+                id="snapchat"
+                placeholder="https://www.snapchat.com/"
+                autoComplete="off"
+                value={social[0].snapchatUrl}
+                onChange={(e) => handleChange("snapchatUrl", e.target.value)}
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="telegram">Telegram</Label>
+              <Input
+                id="telegram"
+                placeholder="https://www.telegram.com/"
+                autoComplete="off"
+                value={social[0].telegramUrl}
+                onChange={(e) => handleChange("telegramUrl", e.target.value)}
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="youtube">Youtube</Label>
+              <Input
+                id="youtube"
+                placeholder="https://www.youtube.com/"
+                autoComplete="off"
+                value={social[0].youtubeUrl}
+                onChange={(e) => handleChange("youtubeUrl", e.target.value)}
+              />
+            </div>
+          </div>
+        )}
+        <div className="flex gap-5 justify-end ">
+          <Button onClick={handleCancel}>Cancel</Button>
+          <Button onClick={handleUpdate}>Update</Button>
+        </div>
+      </div>
+    </>
+  );
+};
